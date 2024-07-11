21e5c79e271dd63fb18f801c3b1723cdcf8bb34c
鹿游原 <115378528+Steven-Zhang98@users.noreply.github.com>
2024-07-11 23:46:43 +0100

    Create 1.py

---
+++ Python/1.py
@@ -0,0 +1,6 @@
+Write a method called printIndexed that accepts a String as its parameter and prints the String's characters in order followed by their indexes in reverse order. For example, the call of printIndexed("ZELDA"); should print Z4E3L2D1A0 to the console.
+?
+1. Print(Z4)
+2. write a loop
+3. write a function
+
